07126c3a0e6f35ab22b6c2d9fb57e47fc1c5d4e3
Ryan Kois <ryan.kois@gmail.com>
2014-05-26 23:54:58 -0700

    Make it so.

---
+++ index.js
@@ -0,0 +1,31 @@
+module.exports = function(bot) {
+  bot.on('msg', function(msg){
+    var regex = /^!r (\w*) ?(\w*)?$/
+    var matches = msg.msg.match(regex)
+    if(!matches) {
+      return
+    }
+
+    var postIndex = matches[2] || 0;
+    var payload = ''
+    req = http.get('http://www.reddit.com/r/' + encodeURI(matches[1].trim()) + '.json', function(res) {
+      if (res.statusCode !== 200) {
+        return
+      }
+      res.on('data', function(chunk){
+        payload += chunk.toString()
+      })
+      res.on('end', function(){
+        var posts = JSON.parse(payload)
+        if(postIndex > posts.data.children.length) {
+          return
+        }
+        var post = posts.data.children[postIndex].data
+
+        console.log(post)
+        var response = msg.sender + ': ' + post.title + ' - ' + post.url
+        bot.msg([msg.chan], response)
+      })
+    })
+  })
+}
