d656e19567d5ac62baa9fcb8b2f149605e8c0b4c
Ryan Kois <ryan.kois@gmail.com>
2014-05-27 00:01:29 -0700

    whoopth.

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
+var http = require('http')
+
 module.exports = function(bot) {
   bot.on('msg', function(msg){
     var regex = /^!r (\w*) ?(\w*)?$/
     var matches = msg.msg.match(regex)
+
     if(!matches) {
       return
     }
 
     var postIndex = matches[2] || 0;
+
     var payload = ''
     req = http.get('http://www.reddit.com/r/' + encodeURI(matches[1].trim()) + '.json', function(res) {
       if (res.statusCode !== 200) {
@@ -17,12 +21,12 @@ module.exports = function(bot) {
       })
       res.on('end', function(){
         var posts = JSON.parse(payload)
+
         if(postIndex > posts.data.children.length) {
           return
         }
-        var post = posts.data.children[postIndex].data
 
-        console.log(post)
+        var post = posts.data.children[postIndex].data
         var response = msg.sender + ': ' + post.title + ' - ' + post.url
         bot.msg([msg.chan], response)
       })
